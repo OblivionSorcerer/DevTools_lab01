e9aa5caebf047ba0add2d10e8254d27d40dd54b7
OblivionSorcerer <Gecon5@mail.ru>
2022-12-07 23:08:39 +0400

    Register and Login Pages have been added

---
+++ NorthwindApp/Northwind.Web.Tests/SeleniumTests/Pages/LogInPage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlElements.Elements;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace Northwind.Web.Tests.SeleniumTests.Pages
+{
+    public class LogIn : HtmlPage
+    {
+        [FindsBy(How = How.Id, Using = "Input.Email")]
+        private HtmlInput email;
+
+        [FindsBy(How = How.Id, Using = "Input.Password")]
+        private HtmlInput password;
+
+        [FindsBy(How = How.CssSelector, Using = "input[type='submit']")]
+        private HtmlInput logInButton;
+
+        public LogIn(ISearchContext webDriverOrWrapper) : base(webDriverOrWrapper)
+        {
+        }
+        public string Email
+        {
+            get { return email.Value; }
+            set { email.SendKeys(value); }
+        }
+        public string Password
+        {
+            get { return password.Value; }
+            set { password.SendKeys(value); }
+        }
+        public LogIn LogInAndGoToMainPage()
+        {
+            logInButton.Click();
+            return PageObjectFactory.Create<LogIn>(this);
+        }
+    }
+}
---
+++ NorthwindApp/Northwind.Web.Tests/SeleniumTests/Pages/RegisterPage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlElements.Elements;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace Northwind.Web.Tests.SeleniumTests.Pages
+{
+    public class RegisterPage : HtmlPage
+    {
+        [FindsBy(How = How.Id, Using = "Input_Email")]
+        private HtmlInput email;
+
+        [FindsBy(How = How.Id, Using = "Input_Password")]
+        private HtmlInput password;
+
+        [FindsBy(How = How.Id, Using = "Input_ConfirmPassword")]
+        private HtmlInput confirmPassword;
+
+        [FindsBy(How = How.Id, Using = "registerSubmit")]
+        private HtmlInput registerButton;
+
+        public RegisterPage(ISearchContext webDriverOrWrapper) : base(webDriverOrWrapper)
+        {
+        }
+        public string Email
+        {
+            get { return email.Value; }
+            set { email.SendKeys(value); }
+        }
+        public string Password
+        {
+            get { return password.Value; }
+            set { password.SendKeys(value); }
+        }
+        public string ConfirmPassword
+        {
+            get { return confirmPassword.Value; }
+            set { confirmPassword.SendKeys(value); }
+        }
+        public MainPage RegisterAndGoToMainPage()
+        {
+            registerButton.Click();
+            return PageObjectFactory.Create<MainPage>(this);
+        }
+    }
+}
